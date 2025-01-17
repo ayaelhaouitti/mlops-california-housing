78b7341597d0929ee8d801608c0a7d85acbe9033
Aya Elhaouitti <ayaelhaouitti@Host-002.lan>
2025-01-17 20:45:00 +0100

    test

--- main_tests.py
+++ main_tests.py
@@ -2,21 +2,19 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import mlflow.pyfunc
 import numpy as np
+import os
 
-# Initialiser l'application FastAPI
 app = FastAPI(
     title="API de Prédiction de Prix Immobiliers",
     description="Cette API utilise un modèle ML pour prédire le prix médian des maisons en Californie.",
     version="1.0"
 )
 
-# Chemin relatif au modèle dans le dépôt GitHub
-model_path = "runs:/3e983990f0f940c9833110571636c3ba/model"
+model_path = "./mlruns/196678121596541976/3e983990f0f940c9833110571636c3ba/artifacts/model"
 
-# Charger le modèle depuis MLflow
+# Charger le modèle depuis ce chemin local
 model = mlflow.pyfunc.load_model(model_path)
 
-# Schéma pour les données d'entrée
 class HousingInput(BaseModel):
     MedInc: float
     HouseAge: float
@@ -27,18 +25,21 @@ class HousingInput(BaseModel):
     Latitude: float
     Longitude: float
 
-# Point de terminaison pour les prédictions
 @app.post("/predict")
 def predict(input_data: HousingInput):
-    # Transformer les données en tableau numpy
-    features = np.array([[input_data.MedInc, input_data.HouseAge, input_data.AveRooms,
-                           input_data.AveBedrms, input_data.Population, input_data.AveOccup,
-                           input_data.Latitude, input_data.Longitude]])
-    # Faire une prédiction
+    features = np.array([[
+        input_data.MedInc,
+        input_data.HouseAge,
+        input_data.AveRooms,
+        input_data.AveBedrms,
+        input_data.Population,
+        input_data.AveOccup,
+        input_data.Latitude,
+        input_data.Longitude
+    ]])
     prediction = model.predict(features)
     return {"prediction": float(prediction[0])}
 
-# Point de terminaison pour le contrôle de santé
 @app.get("/health")
 def health_check():
     return {"status": "UP"}
